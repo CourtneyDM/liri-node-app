315116587cc2316bfc293dc4eb8ead9ecf449de8
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-30 00:05:39 -0500

    added dotenv Node module

--- liri.js
+++ liri.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var request = require("request");
 
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+request("http://www.google.com", function (error, response, body) {
+    console.log("error: " + error);
+    console.log("statusCode: " + response);
+    console.log("body: " + body);
+});
