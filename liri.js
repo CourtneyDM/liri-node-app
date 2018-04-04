4169ae0f59f2ae7a3964f2c50f57b1d24fb167dc
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-04-04 15:12:49 -0500

    added logging functionality; added command to function paramaters; added comments/error messages

--- liri.js
+++ liri.js
@@ -14,28 +14,39 @@ var keys = require("./keys.js");
 var cmd = process.argv[2].toLowerCase();
 var cmdOption = "";
 
+// Create a file to log output
+var logFile = "./log.txt";
+
 // Test the command line option selected by user
 switch (cmd) {
     case "my-tweets":
-        getTweets();
+        getTweets(cmd);
         break;
     case "movie-this":
-        getMovie(cmdOption);
+        getMovie(cmd, cmdOption);
         break;
     case "do-what-it-says":
-        getFileInput();
+        getFileInput(cmd);
         break;
     case "spotify-this-song":
-        getSpotify(cmdOption);
+        getSpotify(cmd, cmdOption);
         break;
     default:
-        console.log("unable to process your request.");
+        console.log("FAILURE: unable to process command.");
         break;
 }
 
 
 // Get Twitter information and display in console
-function getTweets() {
+function getTweets(command) {
+
+    // Append command to the log file
+    fs.appendFile(logFile, "\nCommand: " + command + "\n", error => {
+        if (error) {
+            error = "ERROR: Unable to Log Command.";
+            return console.log(error);
+        }
+    });
 
     // Declare new Twitter object
     var client = new Twitter(keys.twitter);
@@ -46,17 +57,35 @@ function getTweets() {
             return console.log(error);
         }
         var i = 1;
+
+        // Get each tweet from the Tweets Object...
         tweets.forEach(tweet => {
+
+            // ...display each Tweet to the console
             console.log(`Tweet ${i}: ${tweet.text}` + "\n");
+
+            // ...append each Tweet to the log file
+            fs.appendFile(logFile, `Tweet ${i}: ${tweet.text}` + "\n", error => {
+                if (error) {
+                    return console.log("Unable to log Twitter item.");
+                }
+            })
             i++;
         });
     });
-    return;
 }
 
 
 // Get movie information and display in console
-function getMovie(movie) {
+function getMovie(command, movie) {
+
+    // Append command to the log file
+    fs.appendFile(logFile, "\nCommand: " + command + "\n", error => {
+        if (error) {
+            error = "ERROR: Unable to Log Command.";
+            return console.log(error);
+        }
+    });
 
     // If no movie title was provided, set default title to Mr. Nobody 
     if (movie === "") {
@@ -85,40 +114,62 @@ function getMovie(movie) {
     // Send request to OMDB API
     request(queryURL, (error, response, body) => {
 
-        // If an error occurred, return the error
+        // If an error occurred or the movie title cannot be found, return error message
         if ((error) || (!JSON.parse(body).Title)) {
+            error = "ERROR: Movie Title Not Found.";
+            fs.appendFile(logFile, `${movie}: ${error}` + "\n", error => {
+                if (error) {
+                    console.log("ERROR: Error Message Not logged.");
+                }
+            });
             return console.log(error);
         }
+
         // ...otherwise, create a movie Object and set its properties
         else {
             var movieObj = {
-                title: JSON.parse(body).Title,
-                year: JSON.parse(body).Released,
-                IMDB: JSON.parse(body).imdbRating,
-                rotten_tomatoes: JSON.parse(body).Ratings[1].Value,
-                country: JSON.parse(body).Country,
-                language: JSON.parse(body).Language,
-                plot: JSON.parse(body).Plot,
-                actors: JSON.parse(body).Actors
+                Title: JSON.parse(body).Title,
+                Year: JSON.parse(body).Released,
+                IMDB_Rating: JSON.parse(body).imdbRating,
+                Rotten_Tomatoes: JSON.parse(body).Ratings[1].Value,
+                Country: JSON.parse(body).Country,
+                Language: JSON.parse(body).Language,
+                Plot: JSON.parse(body).Plot,
+                Actors: JSON.parse(body).Actors
             };
 
-            // Display movie details in the console
-            console.log(`Title: ${movieObj.title}`);
-            console.log(`Year Released: ${movieObj.year}`);
-            console.log(`IMDB Rating: ${movieObj.IMDB}`);
-            console.log(`Rotten Tomatoes Rating: ${movieObj.rotten_tomatoes}`);
-            console.log(`Country Produced: ${movieObj.country}`);
-            console.log(`Language: ${movieObj.language}`);
-            console.log(`Plot: ${movieObj.plot}`);
-            console.log(`Actors: ${movieObj.actors}`);
+            // Get each item in the movie Object...
+            for (var item in movieObj) {
+
+                // ...display movie Object item to the console
+                console.log(`${item}: ${movieObj[item]}`);
+
+                // ...add the movie Object item to the log file
+                fs.appendFile(logFile, `${item}: ${movieObj[item]}` + "\n", error => {
+                    if (error) {
+                        error = "FAILED: Unable to Log Movie Details.";
+                        return console.log(error);
+                    }
+                });
+            }
+            console.log("SUCCESS: Logged Movie Details.");
         }
     });
+    // End of Request
 }
 
 
 // Get Spotify information and display in console
-function getSpotify(song) {
+function getSpotify(command, song) {
     var spotify = new Spotify(keys.spotify);
+
+    // Append command to the log file
+    fs.appendFile(logFile, "\nCommand: " + command + "\n", error => {
+        if (error) {
+            error = "ERROR: Unable to Log Command."
+            return console.log(error);
+        }
+    });
 
     // If a song tile was not provided, set a default song title
     if (song === "") {
@@ -146,24 +197,41 @@ function getSpotify(song) {
 
         // If an error occurred, return error...
         if (error) {
+            error = "ERROR: Song Title Not Found.";
             return console.log(error);
         }
 
         // ...otherwise, log information to console
         else {
             console.log(data);
+            fs.appendFile(logFile, data, error => {
+                if (error) {
+                    error = "FAILED: Unable to Log Song Details.";
+                    return console.log(error);
+                }
+            });
         }
     });
 }
 
 // Display command from file in console
-function getFileInput() {
+function getFileInput(command) {
     var textFile = "./random.txt";
 
+    // Append command to the log file
+    fs.appendFile(logFile, "\nInitial command: " + command, error => {
+        if (error) {
+            error = "ERROR: Unable to log command.";
+            return console.log(error);
+        }
+    });
+
+    // Read the file stream to get command
     fs.readFile(textFile, "utf8", (error, data) => {
 
-        // If an error occurred, return the error...
+        // If an error occurred on the file read, return the error...
         if (error) {
+            error = "ERROR: Could not read from file";
             return console.log(error);
         }
 
@@ -176,19 +244,19 @@ function getFileInput() {
         var fsCmd = inputArr[0];
         var fsOption = inputArr[1];
 
-        // Test the command read from file stream
+        // Test the command read from file stream and execute corresponding function
         switch (fsCmd) {
             case "my-tweets":
                 getTweets();
                 break;
             case "movie-this":
-                getMovie(fsOption);
+                getMovie(fsCmd, fsOption);
                 break;
             case "spotify-this-song":
-                getSpotify(fsOption);
+                getSpotify(fsCmd, fsOption);
                 break;
             default:
-                console.log("Unable to process request.");
+                console.log("FAILURE: Unable to process command.");
                 break;
         }
     });
