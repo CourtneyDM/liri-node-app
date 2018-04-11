493fafa1f6a9cad9d2ef3fe3b46977ee1f9d96ba
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-04-10 23:04:02 -0500

    Final touches...

--- keys.js
+++ keys.js
@@ -1,8 +1,6 @@
 console.log("this is loaded.");
 var spotifyAPI = require("node-spotify-api");
 
-// var spotify = require("./spotifyKeys.js");
-
 exports.twitter = {
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 // Include Environment Variables
-require("dotenv").config()
+require("dotenv").config();
 
-// Include Node dependencies
+// Include Node Modules
 var fs = require("fs");
 var request = require("request");
 var Spotify = require("node-spotify-api");
@@ -9,7 +9,6 @@ var Twitter = require("twitter");
 
 // Include module that contains Twitter and Spotify access tokens and secrets
 var keys = require("./keys.js");
-// var spotifyAPI = require("./spotifyKeys.js");
 
 // Use the third command line argument as command to execute - indexing is zero-based
 var cmd = process.argv[2].toLowerCase();
@@ -19,116 +18,67 @@ var cmdOption = "";
 var logFile = "./log.txt";
 
 // Test the command line option selected by user
-switch (cmd) {
-    case "my-tweets":
-        getTweets(cmd);
-        break;
-    case "movie-this":
-        getMovie(cmd, cmdOption);
-        break;
-    case "do-what-it-says":
-        getFileInput(cmd);
-        break;
-    case "spotify-this-song":
-        getSpotify(cmd, cmdOption);
-        break;
-    default:
-        console.log("FAILURE: unable to process command.");
-        break;
+function userCommand(cmd, cmdOption) {
+    switch (cmd) {
+        case "my-tweets":
+            getTweets(cmd);
+            break;
+        case "movie-this":
+            movie = cmdOption;
+            getMovie(cmd, cmdOption);
+            break;
+        case "do-what-it-says":
+            getFileInput(cmd);
+            break;
+        case "spotify-this-song":
+            getSpotify(cmd, cmdOption);
+            break;
+        default:
+            console.log("FAILURE: unable to process command.");
+            break;
+    }
+    return cmd;
 }
-
 
 // Get Twitter information
 function getTweets(command) {
-
-    // Append command to the log file
-    fs.appendFile(logFile, "\nCommand: " + command + "\n", error => {
-        if (error) {
-            error = "ERROR: Unable to Log Command.";
-            return console.log(error);
-        }
-    });
-
     // Declare new Twitter object
     var client = new Twitter(keys.twitter);
 
-    // Use GET method to retrieve user's tweets - returns 20 tweets by default
+    // Call to Twitter API
     client.get("statuses/user_timeline", (error, tweets) => {
-        if (error) {
-            return console.log(error);
-        }
-        var i = 1;
+        if (error) return console.log(error);
 
-        // Get each tweet from the Tweets Object...
-        tweets.forEach(tweet => {
-
-            // ...display each Tweet to the console
-            console.log(`Tweet ${i}: ${tweet.text}` + "\n");
-
-            // ...append each Tweet to the log file
-            fs.appendFile(logFile, `Tweet ${i}: ${tweet.text}` + "\n", error => {
-                if (error) {
-                    return console.log("Unable to log Twitter item.");
-                }
-            })
-            i++;
-        });
+        printToScreen(command, tweets);
+        logEntry(command, tweets);
     });
+    return console.log("SUCCESS: Logged Tweets.");
 }
-
 
 // Get OMDB movie information
 function getMovie(command, movie) {
-
-    // Append command to the log file
-    fs.appendFile(logFile, "\nCommand: " + command + "\n", error => {
-        if (error) {
-            error = "ERROR: Unable to Log Command.";
-            return console.log(error);
-        }
-    });
-
-    // If no movie title was provided, set default title to Mr. Nobody 
-    if (movie === "") {
-        if (process.argv.length < 4) {
-            movie = "Mr. Nobody";
-        }
-        else {
-
-            // Loop through the command line arguments to be used in movie title query
-            for (var i = 3; i < process.argv.length; i++) {
-
-                // If there are multiple arguments after the command, add them to the song title
-                if (i >= 3 && i < process.argv.length - 1) {
-                    movie += process.argv[i] + "+";
-                }
-                else {
-                    movie += process.argv[i];
-                }
+    // If no movie title was provided, set default title to Mr. Nobody
+    if (movie === "" && process.argv.length < 4) {
+        movie = "Mr. Nobody";
+    } else {
+        // Loop through the command line arguments to be used in movie title query
+        for (var i = 3; i < process.argv.length; i++) {
+            if (i >= 3 && i < process.argv.length - 1) {
+                movie += process.argv[i] + "+";
+            } else {
+                movie += process.argv[i];
             }
         }
     }
-
     // Declare variable for OMDB URL to be used for API queries
-    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    var queryURL = `http://www.omdbapi.com/?t="${movie}"&y=&plot=short&apikey=trilogy`;
 
-    // Send request to OMDB API
+    // Call to OMDB API
     request(queryURL, (error, response, body) => {
-
         // If an error occurred or the movie title cannot be found, return error message
-        if ((error) || (!JSON.parse(body).Title)) {
-            error = "ERROR: Movie Title Not Found.";
-            fs.appendFile(logFile, `${movie}: ${error}` + "\n", error => {
-                if (error) {
-                    console.log("ERROR: Error Message Not logged.");
-                }
-            });
-            return console.log(error);
-        }
-
-        // ...otherwise, create a movie Object and set its properties
+        if (error || !JSON.parse(body).Title) return console.log(error);
         else {
-            var movieObj = {
+            let movieObj = {
                 Title: JSON.parse(body).Title,
                 Year: JSON.parse(body).Released,
                 IMDB_Rating: JSON.parse(body).imdbRating,
@@ -139,138 +89,127 @@ function getMovie(command, movie) {
                 Actors: JSON.parse(body).Actors
             };
 
-            // Get each item in the movie Object...
-            for (var item in movieObj) {
-
-                // ...display movie Object item to the console
-                console.log(`${item}: ${movieObj[item]}`);
-
-                // ...add the movie Object item to the log file
-                fs.appendFile(logFile, `${item}: ${movieObj[item]}` + "\n", error => {
-                    if (error) {
-                        error = "FAILED: Unable to Log Movie Details.";
-                        return console.log(error);
-                    }
-                });
-            }
-            console.log("SUCCESS: Logged Movie Details.");
+            printToScreen(command, movieObj);
+            logEntry(command, movieObj);
         }
     });
-    // End of Request
+    return console.log("SUCCESS: Logged Movie Details.");
 }
-
 
 // Get Spotify information
 function getSpotify(command, song) {
-    var spotify = new Spotify(keys.spotify);
+    let spotify = new Spotify(keys.spotify);
 
-    // Append command to the log file
-    fs.appendFile(logFile, "\nCommand: " + command + "\n", error => {
-        if (error) {
-            error = "ERROR: Unable to Log Command."
-            return console.log(error);
-        }
-    });
+    let musicObj = { Artist: "", Title: "", Preview: "", Album: "" };
 
     // If a song tile was not provided, set a default song title
-    if (song === "") {
-        if (process.argv.length < 4) {
-            song = "The Sign";
-        }
-
-        else {
-            // Loop through the command line arguements to be used in song title query
-            for (var i = 3; i < process.argv.length; i++) {
-
-                // If there are multiple arguments after the command, add them to the song title
-                if (i >= 3 && i < process.argv.length - 1) {
-                    song += process.argv[i] + "+";
-                }
-                else {
-                    song += process.argv[i];
-                }
+    if (song === "" && process.argv.length < 4) {
+        song = "The Sign";
+    } else {
+        // Loop through the command line arguements to be used in song title query
+        for (var i = 3; i < process.argv.length; i++) {
+            if (i >= 3 && i < process.argv.length - 1) {
+                song += process.argv[i] + "+";
+            } else {
+                song += process.argv[i];
             }
         }
     }
 
-    // Search Spotify for the song specified on command line
-    spotify.search({ type: "track,artist", query: song, limit: 1 }, (error, data) => {
-
-        // If an error occurred, return error...
+    // Call to Spotify API
+    spotify.search({ type: "track", query: song, limit: 1 }, (error, data) => {
         if (error) {
             error = "ERROR: Song Title Not Found.";
             return console.log(error);
+        } else {
+            // ...otherwise, store data in song Object
+            let songData = data.tracks.items[0];
+
+            musicObj.Artist = songData.artists[0].name;
+            musicObj.Title = songData.name;
+            musicObj.Preview = songData.preview_url;
+            musicObj.Album = songData.album.name;
         }
-
-        // ...otherwise, log information to console
-        else {
-            var track = data.tracks.items[0];
-            var artist = track.artists[0].name;
-            var title = track.name;
-            var preview = track.preview_url;
-            var album = track.album.name;
-
-            console.log(`Artist: ${artist}`);
-            console.log(`Song Title: ${title}`);
-            console.log(`Song Preview: ${preview}`);
-            console.log(`Album: ${album}`);
-
-            // Log information to logfile
-            fs.appendFile(logFile, `Song: ${artist}\nTitle: ${title}\nPreview: ${preview}\nAlbum: ${album}` + "\n", error => {
-                if (error) {
-                    error = "FAILED: Unable to Log Song Details.";
-                    return console.log(error);
-                }
-            });
-        }
+        printToScreen(command, musicObj);
+        logEntry(command, musicObj);
     });
+
+    return console.log("SUCCESS: Logged Song Details.");
 }
 
 // Get File Stream command from file
 function getFileInput(command) {
-    var textFile = "./random.txt";
-
-    // Append command to the log file
-    fs.appendFile(logFile, "\nInitial command: " + command, error => {
-        if (error) {
-            error = "ERROR: Unable to log command.";
-            return console.log(error);
-        }
-    });
+    let textFile = "./random.txt";
+    let fsCmd = "";
+    let fsOpt = "";
 
     // Read the file stream to get command
     fs.readFile(textFile, "utf8", (error, data) => {
-
-        // If an error occurred on the file read, return the error...
         if (error) {
             error = "ERROR: Could not read from file";
             return console.log(error);
+        } else {
+            // ...otherwise, create and array to contain file content, increment index when a comma is read
+            let inputArr = data.split(",");
+
+            // Create and initialize variables for file stream command and options based on input array
+            fsCmd = inputArr[0];
+            fsOpt = inputArr[1];
         }
-
-        // ...otherwise, create and array to contain file content, separating items when a comma is reached
-        else {
-            var inputArr = data.split(",");
-        }
-
-        // Create and initialize variables for file stream command and options based on input array
-        var fsCmd = inputArr[0];
-        var fsOption = inputArr[1];
-
         // Test the command read from file stream and execute corresponding function
-        switch (fsCmd) {
-            case "my-tweets":
-                getTweets();
-                break;
-            case "movie-this":
-                getMovie(fsCmd, fsOption);
-                break;
-            case "spotify-this-song":
-                getSpotify(fsCmd, fsOption);
-                break;
-            default:
-                console.log("FAILURE: Unable to process command.");
-                break;
-        }
+        userCommand(fsCmd, fsOpt);
     });
+    return true;
 }
-/********** END OF FILE **********/
+
+// LOG Results to File
+function logEntry(command, result) {
+    fs.appendFile(
+        logFile,
+        '------------------------------\nRequest: "' +
+        command.toUpperCase() +
+        '"\n',
+        error => {
+            if (error) {
+                return console.log("ERROR: Unable to log command.");
+            }
+        }
+    );
+
+    if (command === "my-tweets") {
+        result.forEach(tweet => {
+            fs.appendFile(logFile, `Tweet: ${tweet.text}` + "\n", error => {
+                if (error) return console.log("Unable to log Tweet.");
+            });
+        });
+    }
+    else {
+        for (var item in result) {
+            fs.appendFile(logFile, "\n" + `${item}: ${result[item]}`, error => {
+                if (error) return console.log(`ERROR: Unable to log results`);
+            });
+        }
+    }
+
+    fs.appendFile(logFile, "\n------------------------------\n", error => {
+        if (error) return console.log("ERROR");
+    });
+    return;
+}
+
+// Print Results to Screen
+function printToScreen(command, result) {
+    if (command === "my-tweets") {
+        var i = 1;
+        result.forEach(tweet => {
+            console.log(`Tweet ${i}: ${tweet.text}`);
+            i++;
+        });
+    } else {
+        for (var item in result) {
+            console.log(`${item}: ${result[item]}`);
+        }
+    }
+}
+// Call userCommand to run code
+userCommand(cmd, cmdOption);
