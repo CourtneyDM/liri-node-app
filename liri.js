4755080d74efb657a6aeb5aa62534fd27b3e0729
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-04-13 12:37:39 -0500

    separated logic from  liri.js; updated comments

--- liri.js
+++ liri.js
@@ -1,32 +1,9 @@
 // Include Logic Dependencies
-let logic = require("./logic.js");
+let logic = require("./logic");
 
 // Use the third command line argument as command to execute - indexing is zero-based
 let cmd = process.argv[2].toLowerCase();
 let cmdOption = "";
 
-// Test the command line option selected by user
-function userCommand(cmd, cmdOption) {
-    switch (cmd) {
-        case "my-tweets":
-            logic.getTweets(cmd);
-            break;
-        case "movie-this":
-            movie = cmdOption;
-            logic.getMovie(cmd, cmdOption);
-            break;
-        case "do-what-it-says":
-            logic.getFileInput(cmd);
-            break;
-        case "spotify-this-song":
-            logic.getSpotify(cmd, cmdOption);
-            break;
-        default:
-            console.log("FAILURE: unable to process command.");
-            break;
-    }
-    return cmd;
-}
-
 // Call userCommand to run code
-userCommand(cmd, cmdOption);
+logic.userCommand(cmd, cmdOption);
